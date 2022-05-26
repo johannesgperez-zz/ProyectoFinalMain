c46b180e8290ba9cc6d89f02001c34663bd43065
johannes <johannesgperez@gmail.com>
2022-05-25 22:48:00 -0300

    Cambios en Bootstrap

--- VPIMs/settings.py
+++ VPIMs/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'Base/static')
 ]
 
 # Default primary key field type
